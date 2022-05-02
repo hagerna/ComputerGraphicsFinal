b8ae440b755cd2f377852e39804060570e1d67b6
hagerna <80783566+hagerna@users.noreply.github.com>
2022-05-02 12:19:48 +0200

    semi-functional point light
    - need to change point light position to be in world space not in local space

--- final-group14/music-animation/shaders/pointLightShader.glsl
+++ final-group14/music-animation/shaders/pointLightShader.glsl
@@ -15,7 +15,9 @@ uniform mat4 u_matrixP; // the projection matrix of the camera
 uniform mat3 u_matrixInvTransM;
 varying vec3 v_normal;    // normal to forward to the fragment shader
 
-
+//Point Light Additions
+uniform vec3 u_lightWorldPosition;
+varying vec3 v_surfaceToLight;
 
 //TODO: Add v_texcoord varying
 varying vec2 v_texcoord;
@@ -27,6 +29,9 @@ void main() {
     v_texcoord = a_texcoord;
 
     gl_Position = u_matrixP * u_matrixV * u_matrixM * vec4 (a_position, 1);
+
+    vec3 surfaceWorldPosition = (u_matrixP * vec4 (a_position, 1)).xyz;
+    v_surfaceToLight = u_lightWorldPosition - surfaceWorldPosition;
 }
 
 #endif
@@ -37,10 +42,10 @@ void main() {
 
 precision highp float; //float precision settings
 uniform vec3 u_tint;            // the tint color of this object
-uniform vec3 u_directionalLight;// directional light in world space
-uniform vec3 u_directionalColor;// light color
+uniform vec3 u_pointLightColor; // light color
 uniform vec3 u_ambientColor;    // intensity of ambient light
 varying vec3 v_normal;  // normal from the vertex shader
+varying vec3 v_surfaceToLight;
 
 //TODO: Add v_texcoord varying
 varying vec2 v_texcoord;
@@ -48,11 +53,12 @@ varying vec2 v_texcoord;
 uniform sampler2D u_mainTex;
 
 void main(void){
-    // calculate basic directional lighting
+    // calculate point lighting
     vec3 normal = normalize(v_normal);
-    float diffuse = max(0.0, dot(normal, -u_directionalLight));
-    vec3 diffuseColor = u_directionalColor * diffuse;
-    vec3 ambientDiffuse = u_ambientColor + diffuseColor;
+    vec3 surfaceToLightDirection = normalize(v_surfaceToLight);
+    float light = max(0.0, dot(normal, surfaceToLightDirection));
+    vec3 lightColor = u_pointLightColor * light;
+    vec3 ambientDiffuse = u_ambientColor + lightColor;
     ambientDiffuse = clamp(ambientDiffuse, vec3(0.0,0.0,0.0), vec3(1.0,1.0,1.0));
 
     //TODO: Add texture color sampling
